a469921e1bff5867ed0330f5500a669b7a413daa
QuinntyneBrown <quinntynebrown@gmail.com>
2021-06-02 01:00:43 -0400

    feat(contacts)

--- src/AngularMaterialSchematics.App/src/app/app-routing.module.ts
+++ src/AngularMaterialSchematics.App/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', pathMatch:'full', loadChildren: () => import('./contacts/contacts.module').then(m => m.ContactsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
